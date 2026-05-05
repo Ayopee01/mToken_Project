ce45bbbbe4024c63bcd0086ebdddc91ce02f2886
peeraphol <ayopee001@gmail.com>
2026-05-05 09:52:14 +0700

    update off config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/test2",
-  output: "standalone",
+  // basePath: "/test2",
+  // output: "standalone",
   trailingSlash: false,
 
   images: {
     remotePatterns: [
       // ถ้า URL รูปมีโดเมนอื่น/ http ให้เพิ่มได้ เช่น
       // protocol: "http", 
-      // hostname: "www.tmd.go.th", 
+      // hostname: "example.com", 
       // pathname: "/media/**",
     ],
   },
